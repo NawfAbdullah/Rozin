1b5349d668fa20ecad515e2ff2225a8200598320
Nawf Abdullah <k.nawfabdullah@gmail.com>
2024-05-05 10:25:59 +0530

    v0.3 complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,13 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
     const loggedIn = {
-        firstName:'Adrian'
+        firstName:'Nawf',
+        lastName:'Abdullah',
+        email:'nawfabu@gmail.com'
     }
     return (
         <section className="home">
@@ -24,6 +27,7 @@ const Home = () => {
                     />
                 </header>
             </div>
+            <RightSidebar user={loggedIn} banks={[{currentBalance:123.50},{currentBalance:500}]} transactions={[]}/>
         </section>
     );
 }
